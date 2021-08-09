8fea9a5964aff37151c5d20b6e5c1b617fe82dda
pickledbees <42310888+pickledbees@users.noreply.github.com>
2021-08-09 18:37:03 +0800

    debug live

--- api/getClinicDataFromDB.js
+++ api/getClinicDataFromDB.js
@@ -3,12 +3,34 @@ const MONGO_URI = process.env.MONGODB_URL;
 const MONGO_COLLECTION = process.env.MONGODB_COLLECTION;
 
 async function getClinicDataFromDB(venueId) {
-  const client = new MongoClient(MONGO_URI);
-  await client.connect();
-  const clinicApiCollection = client.db("clinics").collection(MONGO_COLLECTION);
-  const { _id, ...clinic } = await clinicApiCollection.findOne({ venueId });
+  let client;
+  try {
+    client = new MongoClient(MONGO_URI);
+  } catch (e) {
+    throw 1;
+  }
+  try {
+    await client.connect();
+  } catch (e) {
+    throw 2;
+  }
+
+  let clinicApiCollection;
+  try {
+    clinicApiCollection = client.db("clinics").collection(MONGO_COLLECTION);
+  } catch (e) {
+    throw 3;
+  }
+
+  let c;
+  try {
+    const { _id, ...clinic } = await clinicApiCollection.findOne({ venueId });
+    c = clinic;
+  } catch (e) {
+    throw 4;
+  }
   await client.close();
-  return clinic;
+  return c;
 }
 
 module.exports = getClinicDataFromDB;
