011322bb5a2402793dccf51cb2f6bbb7ab5e329d
pickledbees <42310888+pickledbees@users.noreply.github.com>
2021-08-09 18:05:07 +0800

    updated error message

--- controllers/serveFormPage.js
+++ controllers/serveFormPage.js
@@ -5,15 +5,16 @@ async function serveFormPage(req, res) {
   const venueId = req.query.venueId || req.query.state;
 
   if (!venueId) {
-    return res.status(statusCode.NOT_FOUND).send("page not found");
+    return res
+      .status(statusCode.BAD_REQUEST)
+      .send("page not found, invalid venue");
   }
 
   try {
     const { clinicName } = await getClinicDataFromDB(venueId);
     res.render("form", { clinicName });
   } catch (e) {
-    console.error(e);
-    return res.status(statusCode.NOT_FOUND).send("page not found");
+    return res.status(statusCode.NOT_FOUND).send("page not found, " + e);
   }
 }
 
