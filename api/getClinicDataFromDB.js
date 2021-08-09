62f24767b9f0c3dce16cc0b28960b66cdccf7212
pickledbees <42310888+pickledbees@users.noreply.github.com>
2021-08-09 18:54:38 +0800

    remove debug code from mongo api

--- api/getClinicDataFromDB.js
+++ api/getClinicDataFromDB.js
@@ -3,34 +3,16 @@ const MONGO_URI = process.env.MONGODB_URL;
 const MONGO_COLLECTION = process.env.MONGODB_COLLECTION;
 
 async function getClinicDataFromDB(venueId) {
-  let client;
-  try {
-    client = new MongoClient(MONGO_URI);
-  } catch (e) {
-    throw MONGO_URI;
-  }
-  try {
-    await client.connect();
-  } catch (e) {
-    throw 2;
-  }
+  const client = new MongoClient(MONGO_URI);
 
-  let clinicApiCollection;
-  try {
-    clinicApiCollection = client.db("clinics").collection(MONGO_COLLECTION);
-  } catch (e) {
-    throw 3;
-  }
+  await client.connect();
 
-  let c;
-  try {
-    const { _id, ...clinic } = await clinicApiCollection.findOne({ venueId });
-    c = clinic;
-  } catch (e) {
-    throw 4;
-  }
+  const clinicApiCollection = client.db("clinics").collection(MONGO_COLLECTION);
+  const { _id, ...clinic } = await clinicApiCollection.findOne({ venueId });
+
   await client.close();
-  return c;
+
+  return clinic;
 }
 
 module.exports = getClinicDataFromDB;
